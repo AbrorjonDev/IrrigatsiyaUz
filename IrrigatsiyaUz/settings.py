ccaaed78702db3fbfe507dfbca0bab2f0b73957e
AbrorjonDev <axmadov.git@gmail.com>
2021-07-16 11:02:30 +0500

    some installations

--- IrrigatsiyaUz/settings.py
+++ IrrigatsiyaUz/settings.py
@@ -166,8 +166,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'abrorjonaxmadov21@gmail.comm'
-EMAIL_HOST_PASSWORD = 'testing321'
+EMAIL_HOST_USER = 'abrorjonaxmadov21@gmail.com'
+EMAIL_HOST_PASSWORD = 'dell@inspire1188.'
 
 LOGOUT_REDIRECT_URL = '/me/'
 
