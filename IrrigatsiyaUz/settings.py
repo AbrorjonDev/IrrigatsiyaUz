dddde84055df476d6bb1c3ced5493af0493b0438
AbrorjonDev <axmadov.git@gmail.com>
2021-07-18 06:38:19 +0500

    SOME CHANGES

--- IrrigatsiyaUz/settings.py
+++ IrrigatsiyaUz/settings.py
@@ -151,10 +151,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    './static/',
-    # os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static')
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
