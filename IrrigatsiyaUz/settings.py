bf81aeec390ba8468dc81e0eb0a89dccd2aab0a6
AbrorjonDev <axmadov.git@gmail.com>
2021-07-18 06:30:54 +0500

    static files

--- IrrigatsiyaUz/settings.py
+++ IrrigatsiyaUz/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'corsheaders',
     'phonenumber_field',
     'drf_yasg',
+    'whitenoise',
+
 ]
 
 # MIDDLEWARE_CLASSES = (
@@ -49,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -149,7 +152,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    './static/',
+    # os.path.join(BASE_DIR, 'static')
 ]
 
 
