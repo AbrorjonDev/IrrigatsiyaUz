8cc58430051fa7f4d471cadd0b9e115b32d41fea
AbrorjonDev <axmadov.git@gmail.com>
2021-07-29 12:15:55 +0500

    postgres db connected successfully, bugs fixed

--- IrrigatsiyaUz/settings.py
+++ IrrigatsiyaUz/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'IrrigatsiyaUz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'irrigatsiyauz',
+        'HOST':'127.0.0.1',
+        'PORT':5432,
+        'USER':'postgres',
+        'PASSWORD':'testing321',
     }
 }
 
